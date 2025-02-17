d9baeb3097836ce5b327e68226f2d1599685939c
aphex <wenlogic@protonmail.com>
2025-02-17 11:58:00 +0000

    fix broken stories

--- apps/dapp/src/stories/auctions/auction-card-banner.stories.tsx
+++ apps/dapp/src/stories/auctions/auction-card-banner.stories.tsx
@@ -1,9 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { AuctionCard } from "modules/auction/auction-card";
 import { AuctionCardBanner } from "modules/auction/auction-card-banner";
 import { chains } from "@axis-finance/env";
 
 import { environment } from "utils/environment";
+import { getAuctionMock } from "../mocks/auction";
 
 const activeChains = chains.activeChains(environment.isTestnet);
 const chain = activeChains[0];
@@ -15,17 +15,14 @@ const meta = {
 
 export default meta;
 
-type Story = StoryObj<typeof AuctionCard>;
+type Story = StoryObj<typeof AuctionCardBanner>;
+const auction = getAuctionMock();
 
 export const Primary: Story = {
   args: {
     image:
       "https://indieground.net/wp-content/uploads/2023/03/Freebie-GradientTextures-Preview-06.jpg",
     chain,
-    deadline: new Date(1715981309 * 1000),
-    curator: {
-      name: "aphex",
-      avatar: "https://github.com/wenfix.png",
-    },
+    auction,
   },
 };
