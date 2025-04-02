71b688b7c49a9ae4a84efc597e0212d65968cbc7
Burtonium <9215927+Burtonium@users.noreply.github.com>
2025-04-02 13:42:19 +0700

    Fix tooltip

--- apps/token-sale/src/app-config.ts
+++ apps/token-sale/src/app-config.ts
@@ -1,5 +1,11 @@
+const AUCTION_LOT_ID = 1;
+const AUCTION_HOUSE_CONTRACT_ADDRESS =
+  "0xba000020fed3cf3bf473f09ca0a72ba123e20926";
+const AUCTION_CHAIN_NAME = "mainnet";
+
 export const AUCTION_CHAIN_ID = 1;
-export const AUCTION_ID =
-  "mainnet-0xba000020fed3cf3bf473f09ca0a72ba123e20926-1";
+
 export const SUBGRAPH_URL =
   "https://subgraph.satsuma-prod.com/44c4cc082f74/spaces-team/axis-origin-mainnet/version/v1.0.6/api";
+
+export const AUCTION_ID = `${AUCTION_CHAIN_NAME}-${AUCTION_HOUSE_CONTRACT_ADDRESS}-${AUCTION_LOT_ID}`;
