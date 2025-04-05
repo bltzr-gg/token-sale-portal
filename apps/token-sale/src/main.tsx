cfc4c87198220ffaaa10512a522617b88946afcd
Burtonium <9215927+Burtonium@users.noreply.github.com>
2025-04-05 12:20:47 +0700

    Add sentry

--- apps/token-sale/src/main.tsx
+++ apps/token-sale/src/main.tsx
@@ -4,6 +4,11 @@ import "./polyfills";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "./context/router";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://49fcbc65f32961f0c1fb75d4ca83fa05@o4508158923964416.ingest.us.sentry.io/4509098982506496",
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
